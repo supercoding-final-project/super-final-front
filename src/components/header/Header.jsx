ab33601000aaa31181f9fbd7d4e93ce8e257a4fb
laurenCho9 <lauren.choux@gmail.com>
2023-09-01 06:39:33 +0900

    style #11: 메인 헤더 ui 구현
    - 직접 기획 및 제작한 로고 업로드
    - 메인 헤더 ui 완료
    - package.json 기존에 회의한 대로 수정

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,37 @@
+import * as S from './Header.style';
+import logo from '../../logo.svg';
+import search from '../../assets/search.svg';
+
 const Header = () => {
-  return <div>헤더</div>;
+  return (
+    <S.HeaderWrap>
+      <header>
+        <div className="header_start_menu">
+          <S.Logo>
+            <img src={logo} alt="logo" />
+          </S.Logo>
+          <S.Nav>
+            <div className="menu">멘토</div>
+            <div className="menu">코드리뷰</div>
+          </S.Nav>
+        </div>
+        <div className="header_end_menu">
+          <S.Nav>
+            <form>
+              <label>
+                <input type="text" />
+                <button type="button">
+                  <img src={search} alt="search" />
+                </button>
+              </label>
+            </form>
+            <div className="menu">로그인</div>
+            <div className="menu mento">멘토접속</div>
+          </S.Nav>
+        </div>
+      </header>
+    </S.HeaderWrap>
+  );
 };
 
 export default Header;
