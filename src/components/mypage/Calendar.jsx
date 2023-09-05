543e41568dcdde4f70dd724e02d859e46876afbf
jumosd <jumosd@icloud.com>
2023-09-06 08:29:04 +0900

    feat :예약확인정보가 바뀌어 재수정

--- src/components/mypage/Calendar.jsx
+++ src/components/mypage/Calendar.jsx
@@ -16,7 +16,7 @@ function Calendar({ Menti }) {
     //is모달오픈 상태만들기
     const [isOpenModal, setIsOpenModal] = useState(false)
     //멘토이름
-    const [mentoName, setMentoName] = useState([])
+
 
 
     const ThisMonth = new Date().getMonth()
@@ -75,41 +75,40 @@ function Calendar({ Menti }) {
     //달력에 해당하는 유저의 정보를 넣는 프로그램 ㄱㄱ!***************************************************************************
 
     //멘티가 예약신청한 월들을 표시한다
-    let checkReservation = Menti.예약확인
-    //화면에 선택한달
-    let 선택달 = (currentDate.getMonth() + 1).toString().padStart(2, "0")
-    //현재 화면에 선택한달들중에 있는 예약들이다
-    let 선택달의예약들 = (checkReservation[선택달])
-    //멘토님들을 체크한다 멘토님들은 키값이라 키빼서 다시 리스트로 만들어줌
-    let 멘토들객체키 = Object.keys(선택달의예약들);
-    let 멘토님들 = 멘토들객체키.map(item => item);
 
-    //멘토님들을 순회하며 해당 멘토이름을 useState()로 업데이트한다
-    useEffect(() => {
-        for (let i = 0; i < 멘토님들.length; i++) {
-            setMentoName(prev => [...prev, 멘토님들[i]])
-        }
+    const checkReservation = (relevantDay) => {
+        let checkReservation = Menti.예약확인
+        // //화면에 선택한달
+        let selectMonth = (currentDate.getMonth() + 1).toString().padStart(2, '0')
 
-    }, [currentDate])
+        // //현재 화면에 선택한달들중에 있는 예약들이다
+        let 선택달의예약들 = (checkReservation[selectMonth])
+        try {
+            let result = 선택달의예약들[relevantDay]
+            return result
+
+        } catch (error) { }
+    }
+
 
     //새로 만들 멘토예약일 객체 ==> 멘토이름: [날짜들]
     // ex) {홍길동: [1,3,15,20]}
-    let 멘토예약일 = {};
-    for (let i = 0; i < mentoName.length; i++) {
-        let 멘토이름 = mentoName[i];
-        let 멘토별예약일자 = 선택달의예약들[멘토이름];
+    // let 멘토예약일 = {};
+    // for (let i = 0; i < mentoName.length; i++) {
+    //     let 멘토이름 = mentoName[i];
+    //     let 멘토별예약일자 = 선택달의예약들[멘토이름];
 
-        멘토별예약일자.forEach(item => {
-            let newitem = item.split('-');
-            let 예약일 = parseInt(newitem[2]).toString();
-            if (!멘토예약일[멘토이름]) {
-                멘토예약일[멘토이름] = [];
-            }
-            if (!멘토예약일[멘토이름].includes(예약일)) {
-                멘토예약일[멘토이름].push(예약일);
-            }
-        });
-    }
+    //     멘토별예약일자.forEach(item => {
+    //         let newitem = item.split('-');
+    //         let 예약일 = parseInt(newitem[2]).toString();
+    //         if (!멘토예약일[멘토이름]) {
+    //             멘토예약일[멘토이름] = [];
+    //         }
+    //         if (!멘토예약일[멘토이름].includes(예약일)) {
+    //             멘토예약일[멘토이름].push(예약일);
+    //         }
+    //     });
+    // }
 
 
 
@@ -164,35 +163,41 @@ function Calendar({ Menti }) {
 
 
                 {calendarDays.map((day, index) => {
-                    let strDay = String(day)
                     // 예약 정보가 있는지 확인
-                    if (멘토예약일) {
-                        // 해당 날짜에 예약 정보가 있는 멘토들의 이름을 가져오기
-                        const 멘토들의예약 = Object.keys(멘토예약일).filter(멘토이름 => 멘토예약일[멘토이름].includes(strDay));
-                        // console.log(멘토들의예약)
-                        return (
-                            <S.DayCell key={index} $empty={day === null} onClick={() => openModalHandler(day)}>
-                                {day !== null ? (
-                                    <>
-                                        {day}
-                                        {멘토들의예약.map((멘토이름, idx) => (
-                                            <S.CellText key={idx}>
-                                                {멘토이름}멘토 예약
-                                            </S.CellText>
-                                        ))}
-                                    </>
-                                ) : null}
-                            </S.DayCell>
-                        );
-                        // 해당 날짜에 예약이 있는 경우 멘토 이름 표시
+
+                    if (checkReservation) {
+                        const mentos = checkReservation(day)
+                        if (mentos) {
+
+                            return (
+                                <S.DayCell key={index} $empty={day === null} onClick={() => openModalHandler(day)}>
+                                    {day !== null ? (
+                                        <>
+                                            {day}
+                                            {mentos.map((멘토이름, idx) => (
+                                                <S.CellText key={idx}>
+                                                    {멘토이름}멘토 예약
+                                                </S.CellText>
+                                            ))}
+                                        </>
+                                    ) : null}
+                                </S.DayCell>
+                            );
+                        }
                     }
+
+
+
+                    // 해당 날짜에 예약이 있는 경우 멘토 이름 표시
+
                     // 예약 정보가 없거나 예약되지 않은 경우 빈 칸 표시
                     return (
                         <S.DayCell key={index} $empty={day === null} onClick={() => openModalHandler(day)}>
                             {day !== null ? (
                                 <>
+                                    {day}
                                     <S.CellText>
-                                        빈칸임
+
                                     </S.CellText>
                                 </>
                             ) : null}
