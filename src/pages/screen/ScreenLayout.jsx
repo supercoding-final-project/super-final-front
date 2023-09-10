053a2b7fbf8efa0980d8bd94b1f83d7091248559
Sim <0doogadooga@gmail.com>
2023-09-10 23:22:36 +0900

    임시 커밋

--- src/pages/screen/ScreenLayout.jsx
+++ src/pages/screen/ScreenLayout.jsx
@@ -1,6 +1,75 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+// import { OpenVidu } from 'openvidu-browser';
+// import { useEffect, useState } from 'react';
 import * as S from './screen.style';
 
 const ScreenLayout = () => {
+  const [session, setSession] = useState(null);
+  // console.log(session);
+
+  const fetchDataID = async () => {
+    const data = {
+      mediaMode: 'ROUTED',
+      recordingMode: 'MANUAL',
+      customSessionId: '',
+    };
+
+    const base64 = btoa('openviduapp:MY_SECRET');
+    console.log(base64);
+
+    try {
+      const response = await axios.post('http://54.180.86.41:8080/api/v1/video/create', data, {
+        headers: {
+          // Authorization: `Basic ${base64}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log('Authorization :', `Basic ${base64}`);
+      console.log('성공 :', response.data);
+      // localStorage.setItem('sessionId', response.data);
+      setSession(response.data.data);
+    } catch (error) {
+      console.error('에러 :', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDataID();
+  }, []);
+
+  const fetchDataConnection = async () => {
+    const connection = {
+      type: 'WEBRTC',
+      data: '심채운',
+      role: 'PUBLISHER',
+    };
+
+    try {
+      console.log(session);
+      const getResponse = axios.post(
+        `http://54.180.86.41:8080/api/v1/video/enter/${session}`,
+        connection,
+        {
+          header: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      console.log('성공 :', getResponse);
+    } catch (error) {
+      console.error('get 에러 :', error);
+    }
+  };
+
+  useEffect(() => {
+    if (session !== null) {
+      fetchDataConnection();
+    }
+  }, [session]);
+
   return (
     <S.ScreenWrap>
       <div className="screenBox">
