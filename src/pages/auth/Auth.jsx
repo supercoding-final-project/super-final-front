9a620893d8723bdbfc42f30f3f8f51559684d2b4
MoYuBin <bin8727@naver.com>
2023-09-11 17:54:48 +0900

    feat #28: 카카오 로그인 호출(에러)

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -2,13 +2,20 @@ import ClosePath from '@/assets/close.svg';
 import * as S from './Auth.style';
 
 const Auth = () => {
+  const kakaoLogin = () => {
+    window.Kakao.Auth.authorize({
+      redirectUri: `${import.meta.env.VITE_BASE_URL}api/v1/auth/login/kakao`,
+      scope: 'account_email, gender',
+    });
+  };
+
   return (
     <S.Container>
       <S.Wrap>
         <S.Title>소셜 계정으로 로그인!</S.Title>
 
         <S.IconBox>
-          <S.Kakao />
+          <S.Kakao onClick={kakaoLogin} />
 
           <S.Google />
         </S.IconBox>
