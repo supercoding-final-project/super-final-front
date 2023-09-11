d7316da9232330a5eb38525898b7a14201312fdd
Joojue <128790356+Joojue@users.noreply.github.com>
2023-09-12 03:15:27 +0900

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import jsconfigPaths from 'vite-jsconfig-paths';
-import svgr from 'vite-plugin-svgr';
-import react from '@vitejs/plugin-react-swc';
-
-export default defineConfig({
-  plugins: [react(), svgr(), jsconfigPaths()],
-});
