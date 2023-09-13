0cdf55330b8db53c9764e0e723b2f1ab1a672477
jumosd <jumosd@icloud.com>
2023-09-13 15:00:57 +0900

    feat: 요일별로 예약시간설정하기

--- src/components/mypage/mento/Reservation.jsx
+++ src/components/mypage/mento/Reservation.jsx
@@ -1,72 +1,67 @@
-import { useState } from 'react'
-import * as S from "src/components/mypage/mento/Reservation.style"
+import React, { useState } from 'react';
+
+import * as S from './Reservation.style';
 
 const Reservation = () => {
     const reservationTime = [
-        "0000", "0100", "0200", "0300", "0400", "0500", "0600", "0700", "0800", "0900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100", "2200", "2300",
-    ]
+        '0000', '0100', '0200', '0300', '0400', '0500', '0600', '0700', '0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800', '1900', '2000', '2100', '2200', '2300',
+    ];
+
+    const [selectedDay, setSelectedDay] = useState('평일');
     const [selectedTimes, setSelectedTimes] = useState([]);
-    //평일선택
-    const [selectWeekDay, setSelectWeekDay] = useState(false);
-    //주말선택
-    const [selectWeekendDay, setSelectWeekendDay] = useState(false);
-    //요일선택
 
-    const selectItem = (index) => {
-        setSelectedTimes([reservationTime[index]])
-    }
+    const handleDayClick = (day) => {
+        setSelectedDay(day);
+        setSelectedTimes([]); // 요일이 변경되면 선택한 시간 초기화
+    };
 
-    const mouseMove = (index) => {
-        if (selectedTimes.length) {
-            console.log(reservationTime[index])
-
-            setSelectedTimes(prev => [...prev, reservationTime[index]]
-            )
+    const handleTimeClick = (time) => {
+        // 이미 선택한 시간인지 확인
+        if (selectedTimes.includes(time)) {
+            setSelectedTimes(selectedTimes.filter((t) => t !== time)); // 시간이 이미 선택되어 있으면 선택 취소
+        } else {
+            setSelectedTimes([...selectedTimes, time]); // 시간이 선택되어 있지 않으면 추가
         }
+    };
 
+    const handleReservation = () => {
+        // 선택한 요일과 시간을 출력
+        console.log(`요일: ${selectedDay}, 시간: ${selectedTimes.join(', ')} 예약됨`);
     }
 
-    const mouseUp = () => {
-        setSelectedTimes([])
-    }
-
-
-    const selectWeekDayHandler = () => {
-        setSelectWeekDay(prev => !prev)
-    }
-    const setSelectWeekendDayHandler = () => {
-        setSelectWeekendDay(prev => !prev)
-    }
     return (
         <>
-
-
             <div>달력이 보여짐</div>
             <S.DivFlex>
-                <S.Weekdays onClick={selectWeekDayHandler}>평일</S.Weekdays>
-                <S.Weekend onClick={setSelectWeekendDayHandler}>주말</S.Weekend>
+                <S.Weekdays $isSelected={selectedDay === '평일'} onClick={() => handleDayClick('평일')}>평일</S.Weekdays>
+                <S.Weekend $isSelected={selectedDay === '주말'} onClick={() => handleDayClick('주말')}>주말</S.Weekend>
             </S.DivFlex>
+
             <S.DivGrid7>
-                <S.Sunday selectWeekendDay={selectWeekendDay}>일</S.Sunday>
-                <S.Weekdays selectWeekDay={selectWeekDay}>월</S.Weekdays>
-                <S.Weekdays selectWeekDay={selectWeekDay}>화</S.Weekdays>
-                <S.Weekdays selectWeekDay={selectWeekDay}>수</S.Weekdays>
-                <S.Weekdays selectWeekDay={selectWeekDay}>목</S.Weekdays>
-                <S.Weekdays selectWeekDay={selectWeekDay}>금</S.Weekdays>
-                <S.Saturday selectWeekendDay={selectWeekendDay}>토</S.Saturday>
+                <S.Sunday $selectWeekendDay={selectedDay === '일'} onClick={() => handleDayClick('일')}>일</S.Sunday>
+                <S.Weekdays $selectWeekendDay={selectedDay === '월'} onClick={() => handleDayClick('월')}>월</S.Weekdays>
+                <S.Weekdays $selectWeekendDay={selectedDay === '화'} onClick={() => handleDayClick('화')}>화</S.Weekdays>
+                <S.Weekdays $selectWeekendDay={selectedDay === '수'} onClick={() => handleDayClick('수')}>수</S.Weekdays>
+                <S.Weekdays $selectWeekendDay={selectedDay === '목'} onClick={() => handleDayClick('목')}>목</S.Weekdays>
+                <S.Weekdays $selectWeekendDay={selectedDay === '금'} onClick={() => handleDayClick('금')}>금</S.Weekdays>
+                <S.Saturday $selectWeekendDay={selectedDay === '토'} onClick={() => handleDayClick('토')}>토</S.Saturday>
             </S.DivGrid7>
 
-            <S.DivGrid>
-                {reservationTime.map((item, index) => <S.ReservationTime key={index}
-                    onMouseDown={() => selectItem(index)}
-                    onMouseMove={() => mouseMove(index)}
-                    onMouseUp={() => mouseUp(index)}
+            <div>
+                <S.DivGrid>
+                    {reservationTime.map((time, index) => (
+                        <S.ReservationTime
+                            key={index}
+                            $isSelected={selectedTimes.includes(time)} // 시간이 선택되어 있는지 확인
+                            onClick={() => handleTimeClick(time)}
+                        >
+                            {time}
+                        </S.ReservationTime>
+                    ))}
+                </S.DivGrid>
+            </div >
 
-
-                >{item}</S.ReservationTime>)}
-            </S.DivGrid >
-
-
+            <button onClick={handleReservation}>시간예약</button>
         </>
     );
 };
--- src/components/mypage/mento/Reservation.style.js
+++ src/components/mypage/mento/Reservation.style.js
@@ -27,31 +27,31 @@ text-align: center;
 
 export const Sunday = styled.div`
     cursor: pointer;
-    color: ${props => (props.selectWeekendDay ? "Black" : "White")};
+    color: ${props => (props.$selectWeekendDay ? "Black" : "White")};
     padding: 10px 25px;
     border-radius: 5px;
     font-size: larger;
     margin-left: 1rem;
-    background-color: ${props => (props.selectWeekendDay ? "#3dff81" : "Red")};
+    background-color: ${props => (props.$selectWeekendDay ? "#3dff81" : "Red")};
 `
 export const Saturday = styled.div`
     cursor: pointer;
-    color: ${props => (props.selectWeekendDay ? "Black" : "White")};
+    color: ${props => (props.$selectWeekendDay ? "Black" : "White")};
     padding: 10px 25px;
     border-radius: 5px;
     font-size: larger;
     margin-left: 1rem;
-    background-color: ${props => (props.selectWeekendDay ? "#3dff81" : "Blue")};
+    background-color: ${props => (props.$selectWeekendDay ? "#3dff81" : "Blue")};
 `
 
 
 export const Weekdays = styled.div`
-    cursor: pointer;
-    padding: 10px 25px;
-    border-radius: 5px;
-    font-size: larger;
-    margin-left: 1rem;
-    background-color: ${props => (props.selectWeekDay ? "#3dff81" : "aliceblue")}
+cursor: pointer;
+padding: 10px 25px;
+border-radius: 5px;
+font-size: larger;
+margin-left: 1rem;
+background-color: ${props => (props.$selectWeekendDay ? "#3dff81" : "aliceblue")};
 `
 
 export const Weekend = styled.div`
@@ -60,13 +60,14 @@ padding: 10px 25px;
 border-radius: 5px;
 font-size: larger;
 margin-left: 1rem;
-background-color: aliceblue
+background-color: ${props => (props.$selectWeekendDay ? "#3dff81" : "aliceblue")};
 `
 
 export const ReservationTime = styled.div`
 cursor: pointer;
 padding: 10px 25px;
 border-radius: 5px;
-
-background-color: aliceblue;
+background-color: ${props => (props.$isSelected ? "#3dff81" : "aliceblue")};
 `
+
+//요일별로 시간을 예약한곳?
