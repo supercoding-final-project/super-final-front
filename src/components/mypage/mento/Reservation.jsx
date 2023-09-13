6f4ae4752d226b8155a5d1e0598f1a93ba74a385
jumosd <jumosd@icloud.com>
2023-09-14 04:20:38 +0900

    feat: 멘토 구매,주문 내역 커밋

--- src/components/mypage/mento/Reservation.jsx
+++ src/components/mypage/mento/Reservation.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 import * as S from './Reservation.style';
 
@@ -7,36 +7,49 @@ const Reservation = () => {
         '0000', '0100', '0200', '0300', '0400', '0500', '0600', '0700', '0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800', '1900', '2000', '2100', '2200', '2300',
     ];
 
-    const [selectedDay, setSelectedDay] = useState('평일');
-    const [selectedTimes, setSelectedTimes] = useState([]);
+    // 요일별 선택한 시간 배열
+    const [selectedTimes, setSelectedTimes] = useState({
+        일: [],
+        월: [],
+        화: [],
+        수: [],
+        목: [],
+        금: [],
+        토: [],
+    });
 
+    // 선택한 요일 상태
+    const [selectedDay, setSelectedDay] = useState("일");
+
+    // 요일 클릭 핸들러
     const handleDayClick = (day) => {
         setSelectedDay(day);
-        setSelectedTimes([]); // 요일이 변경되면 선택한 시간 초기화
     };
 
+    // 시간 클릭 핸들러
     const handleTimeClick = (time) => {
-        // 이미 선택한 시간인지 확인
-        if (selectedTimes.includes(time)) {
-            setSelectedTimes(selectedTimes.filter((t) => t !== time)); // 시간이 이미 선택되어 있으면 선택 취소
-        } else {
-            setSelectedTimes([...selectedTimes, time]); // 시간이 선택되어 있지 않으면 추가
-        }
+        setSelectedTimes((prev) => ({
+            ...prev,
+            [selectedDay]: prev[selectedDay].includes(time)
+                ? prev[selectedDay].filter((t) => t !== time)
+                : [...prev[selectedDay], time],
+        }));
     };
 
+    // 예약 버튼 클릭 핸들러
     const handleReservation = () => {
-        // 선택한 요일과 시간을 출력
-        console.log(`요일: ${selectedDay}, 시간: ${selectedTimes.join(', ')} 예약됨`);
-    }
 
+        console.log(`요일: ${selectedDay}, 시간: ${selectedTimes[selectedDay].join(', ')} 예약됨`);
+    };
+
+    console.log(selectedTimes)
     return (
         <>
             <div>달력이 보여짐</div>
             <S.DivFlex>
-                <S.Weekdays $isSelected={selectedDay === '평일'} onClick={() => handleDayClick('평일')}>평일</S.Weekdays>
-                <S.Weekend $isSelected={selectedDay === '주말'} onClick={() => handleDayClick('주말')}>주말</S.Weekend>
+                <S.Weekdays>평일</S.Weekdays>
+                <S.Weekend>주말</S.Weekend>
             </S.DivFlex>
-
             <S.DivGrid7>
                 <S.Sunday $selectWeekendDay={selectedDay === '일'} onClick={() => handleDayClick('일')}>일</S.Sunday>
                 <S.Weekdays $selectWeekendDay={selectedDay === '월'} onClick={() => handleDayClick('월')}>월</S.Weekdays>
@@ -52,14 +65,14 @@ const Reservation = () => {
                     {reservationTime.map((time, index) => (
                         <S.ReservationTime
                             key={index}
-                            $isSelected={selectedTimes.includes(time)} // 시간이 선택되어 있는지 확인
+                            $isSelected={selectedTimes[selectedDay].includes(time)}
                             onClick={() => handleTimeClick(time)}
                         >
                             {time}
                         </S.ReservationTime>
                     ))}
                 </S.DivGrid>
-            </div >
+            </div>
 
             <button onClick={handleReservation}>시간예약</button>
         </>
