c1ac94044295354e39f46bb5ff737f1ed4557f79
laurenCho9 <lauren.choux@gmail.com>
2023-09-14 22:16:55 +0900

    style #47 메인페이지 리디자인 UI 작업

--- src/fonts/index.js
+++ src/fonts/index.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 import PretendardWoff2 from './PretendardVariable.woff2';
+import { theme } from 'src/globalLayout/GlobalStyle';
 
 export default createGlobalStyle`
 ${reset}
@@ -11,5 +12,7 @@ ${reset}
     }
     body {
         font-family: 'Pretendard', sans-serif; 
+        background-color: ${theme.color.bgc1};
     }
+    a{color:inherit; text-decoration:none;}
 `;
