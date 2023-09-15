7b9e00443ffc9db39db852d9ae53bc50f3e70f69
Joojue <128790356+Joojue@users.noreply.github.com>
2023-09-16 00:22:07 +0900

    토큰 훅 수정

--- src/hooks/useJwt.js
+++ src/hooks/useJwt.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 function useJwtToken() {
   const [jwtToken, setJwtToken] = useState(null);
+  const [decodedToken, setDecodedToken] = useState(null);
 
   useEffect(() => {
     function getJwtTokenFromCookie() {
@@ -12,7 +13,7 @@ function useJwtToken() {
 
       cookies.forEach((cookie) => {
         const [name, value] = cookie.trim().split('=');
-        if (name === 'access_token') {
+        if (name === 'jwtToken') {
           token = value;
         }
       });
@@ -24,19 +25,23 @@ function useJwtToken() {
     setJwtToken(token);
   }, []);
 
-  const parseJwtToken = () => {
-    try {
-      const decodedToken = jwtDecode(jwtToken);
-      return decodedToken;
-    } catch (error) {
-      console.error('JWT 토큰 파싱 오류', error);
-      return null;
+  useEffect(() => {
+    if (jwtToken) {
+      try {
+        const decodedToken = jwtDecode(jwtToken);
+        setDecodedToken(decodedToken);
+      } catch (error) {
+        console.error('JWT 토큰 파싱 오류', error);
+        setDecodedToken(null);
+      }
+    } else {
+      setDecodedToken(null);
     }
-  };
+  }, [jwtToken]);
 
   return {
     jwtToken,
-    decodedToken: parseJwtToken(),
+    decodedToken,
   };
 }
 
