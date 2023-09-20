6c48f2ce4e7914a7d65a0af7e789359bb679bf1f
Joojue <128790356+Joojue@users.noreply.github.com>
2023-09-20 18:44:31 +0900

    리뷰 조회

--- src/components/detail/post/review/ReviewCard.jsx
+++ src/components/detail/post/review/ReviewCard.jsx
@@ -4,12 +4,13 @@ import * as S from 'src/pages/detail/DetailLayout.style';
 const ReviewCard = (props) => {
   return (
     <S.ReviewCardWrap>
-      <S.Name>{props.data.name}</S.Name>
-      <S.Text>{props.data.review}</S.Text>
+      <S.Name>{props.data.nickname}</S.Name>
+      <S.Text>{props.data.content}</S.Text>
       <S.Rating>
         <Icon name="Star" color="white" />
-        {props.data.rating}
+        {props.data.star}
       </S.Rating>
+      {/* 회차 정보 입력 */}
     </S.ReviewCardWrap>
   );
 };
