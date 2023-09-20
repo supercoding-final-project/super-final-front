0b897d6e6e0085b89c0a9cb7a1c54165c6975c58
Joojue <128790356+Joojue@users.noreply.github.com>
2023-09-20 23:52:01 +0900

    리뷰 등록 최초

--- src/components/detail/post/review/ReviewRegister.jsx
+++ src/components/detail/post/review/ReviewRegister.jsx
@@ -1,21 +1,66 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Icon } from 'src/components/common/icon/Icon';
+import useJwtToken from 'src/hooks/useJwt';
 import * as S from 'src/pages/detail/DetailLayout.style';
 
 const ReviewRegister = (props) => {
+  const { jwtToken } = useJwtToken();
+  const [review, setReview] = useState({
+    orderSheetId: props.orderSheetId,
+    content: '',
+    star: 5,
+  });
+
+  const postReview = () => {
+    axios.post(
+      'https://codevelop.store/api/v1/reviews',
+      {
+        review,
+      },
+      {
+        headers: {
+          Authorization: jwtToken,
+        },
+      },
+    );
+    console.log(review);
+  };
+
+  const contentHandler = (e) => {
+    setReview((prevData) => ({
+      ...prevData,
+      content: e.target.value,
+    }));
+  };
+
+  const starHandler = (selectedStar) => {
+    // 별점 선택 핸들러
+    setReview((prevData) => ({
+      ...prevData,
+      star: selectedStar,
+    }));
+  };
   return (
     <S.ReviewPost>
       <S.ReviewPostHeader>
         <span>{props.title}</span>
         <div>
-          <Icon name="Star" />
-          <Icon name="Star" />
-          <Icon name="Star" />
-          <Icon name="Star" />
-          <Icon name="Star" />
+          {[1, 2, 3, 4, 5].map((starValue) => (
+            <Icon
+              key={starValue}
+              name="Star"
+              onClick={() => starHandler(starValue)} // 클릭 시 별점 설정
+              style={{
+                color: starValue <= review.star ? 'yellow' : 'gray', // 선택된 별까지 색깔 변경
+                cursor: 'pointer',
+              }}
+            />
+          ))}
         </div>
       </S.ReviewPostHeader>
       <S.Text>
-        <textarea placeholder="한줄 리뷰를 입력해주세요." />
+        <textarea placeholder="한줄 리뷰를 입력해주세요." onChange={contentHandler} />
       </S.Text>
       <button>리뷰 등록</button>
     </S.ReviewPost>
