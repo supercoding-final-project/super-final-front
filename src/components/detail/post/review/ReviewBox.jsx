6f7f730a91856557a05fc6b7f041153745887397
Joojue <128790356+Joojue@users.noreply.github.com>
2023-09-21 22:04:05 +0900

    공통 로직 통합

--- src/components/detail/post/review/ReviewBox.jsx
+++ src/components/detail/post/review/ReviewBox.jsx
@@ -1,6 +1,33 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import useJwtToken from 'src/hooks/useJwt';
+
 import * as S from './Review.style';
 import ReviewList from './ReviewList';
 const ReviewBox = (props) => {
+  const [data, setData] = useState([]);
+  const [cursor, setCursor] = useState(0);
+  const { jwtToken } = useJwtToken();
+
+  const getMyReviews = async () => {
+    const res = await axios.get(
+      `https://codevelop.store/api/v1/reviews/${props.endPoint}?cursor=${cursor}&pageSize=5`,
+      {
+        headers: {
+          Authorization: jwtToken,
+        },
+      },
+    );
+    setData(res.data.data.content);
+  };
+
+  const onReviewChange = async () => {
+    await getMyReviews();
+  };
+
+  useEffect(() => {
+    getMyReviews();
+  }, [jwtToken]);
   return (
     <S.ReviewBoxWrap>
       <h1>{props.title}</h1>
@@ -12,13 +39,13 @@ const ReviewBox = (props) => {
             <div key={i}>{option}</div>
           ))}
         </S.BoxHeader>
-        {props.data.map((info, i) => (
+        {data.map((info, i) => (
           <ReviewList
             key={i}
             info={info}
             btnValue={props.btnValue}
             type={props.type}
-            onReviewChange={props.onReviewChange}
+            onReviewChange={onReviewChange}
           />
         ))}
       </S.ReviewBox>
--- src/hooks/useGetMyReviews.js
+++ src/hooks/useGetMyReviews.js
@@ -1,19 +1,32 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import useJwtToken from './useJwt';
 
-export const useGetMyReviews = async (endPoint, setData) => {
+export const useGetMyReviews = (endPoint, setData) => {
   const { jwtToken } = useJwtToken();
   const [cursor, setCursor] = useState(0);
-  const res = await axios.get(
-    `https://codevelop.store/api/v1/reviews/${endPoint}?cursor=${cursor}&pageSize=5`,
-    {
-      headers: {
-        Authorization: jwtToken,
-      },
-    },
-  );
-  setData(res.data.data.content);
-  return useGetMyReviews;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `https://codevelop.store/api/v1/reviews/${endPoint}?cursor=${cursor}&pageSize=5`,
+          {
+            headers: {
+              Authorization: jwtToken,
+            },
+          },
+        );
+        setData(res.data.data.content);
+      } catch (error) {
+        // 오류 처리
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData(); // fetchData 함수를 호출하여 데이터를 가져옵니다.
+  }, [endPoint, cursor, setData, jwtToken]);
+
+  return useGetMyReviews; // fetchData 함수를 반환
 };
